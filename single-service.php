de22bb0c145ecb2f1f42a36899554a75cece1eef
Marcus Battle <marcusbattle@Marcbook.local>
2015-05-07 02:04:35 -0400

    Making the system responsive

--- single-service.php
+++ single-service.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<?php wp_head(); ?>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<div class="container-fluid" style="max-width: 600px;">
+		<div class="container-fluid">
 			<h1><?php echo get_the_title(); ?></h1>
 			<?php echo wpautop( $post->post_content ); ?>
 			<form method="POST">
