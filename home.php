dc64d964158238016d9dd5898a65833930a51f74
Marcus Battle <marcusbattle@Marcbook.local>
2015-05-08 11:10:09 -0400

    Pages now responsive

--- home.php
+++ home.php
@@ -1,7 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head></head>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<?php wp_head(); ?>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</head>
 	<body>
 		<?php echo the_content(); ?>
 	</body>
 </html>
+
+asda
--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head></head>
+	<head>
+		<meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<?php wp_head(); ?>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	</head>
 	<body>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
