616c1d08b86f09941d871783a0a72060589b259c
Marcus Battle <marcusbattle@Marcbook.local>
2015-05-08 11:09:32 -0400

    Added views for pages

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 <html lang="en">
 	<head></head>
 	<body>
-		<?php echo the_content(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+	 		<?php the_content(); ?>
+
+	<?php endwhile; else : ?>
+
+
+ 		<!-- REALLY stop The Loop. -->
+ 	<?php endif; ?>
 	</body>
 </html>
